df2ad33c0be86fa4f8c48d72a221319c86643eb2
Luis Rodero-Merino <luis.rod@gmail.com>
2016-02-28 16:13:22 +0100

    Small fix so clearAllData.sh also removes DataNode2/tmp/*

--- clearAllData.sh
+++ clearAllData.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-./stopAll.sh
-./NameNode/formatFS.sh
+./stopHDFS.sh
 rm -fr ./DataNode1/data/*
+rm -fr ./DataNode1/log/*
+rm -fr ./DataNode1/pid/*
+rm -fr ./DataNode1/tmp/*
 rm -fr ./DataNode2/data/*
+rm -fr ./DataNode2/log/*
+rm -fr ./DataNode2/pid/*
+rm -fr ./DataNode2/tmp/*
+./NameNode/formatFS.sh
 
